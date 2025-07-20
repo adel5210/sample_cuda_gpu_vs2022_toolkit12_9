db304f4ca26285f113f5a57c63c9162a332e9ed9
Adel <adel_5210m@yahoo.com>
2025-07-20 14:24:20 +0800

    Init commit

---
+++ sample_cuda_gpu_vs2022_toolkit12_9/DevSample.cu
@@ -0,0 +1,47 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <array>
+#include <vector>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+
+#define N 1000000
+
+__global__ void add(int* a, int* b) {
+	for (int i = 0; i < N; i++) {
+		a[i] += b[i];
+	}
+}
+
+int main()
+{
+	printf("Run CUDA samples\n");
+
+	std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+	int* a;
+	int* b;
+
+	//allocate unified memory
+	cudaMallocManaged(&a, N * sizeof(int));
+	cudaMallocManaged(&b, N * sizeof(int));
+
+	for (size_t i = 0; i < N; i++)
+	{
+		a[i] = std::rand() % 100;
+		b[i] = std::rand() % 100;
+	}
+
+	add << <1, 1 >> > (a, b);
+
+	//Block until the kernel is done
+	cudaDeviceSynchronize();
+
+	// Free the unified memory
+	cudaFree(a);
+	cudaFree(b);
+
+	return 0;
+}
